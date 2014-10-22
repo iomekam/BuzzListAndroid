68995f5873e10a05d960843e61161cfe463c7344
Ikenna <iomekam3@gatech.edu>
2014-10-22 16:06:36 -0400

    Fixed dependency issues

--- src/com/buzzlist/fragments/PostItemFragment.java
+++ src/com/buzzlist/fragments/PostItemFragment.java
@@ -29,6 +29,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class PostItemFragment extends Fragment {
